0c2f49190915f0430519e9ca8a5ac8e16c933ef4
Iliya <iliya.nedelchev@devocean.solutions>
2020-12-29 00:40:04 +0200

    initial commit

---
+++ Clicker/Clicker/Clicker.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace Clicker
+{
+    partial class Clicker
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(24, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "F8 for left click";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(17, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 25);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "F9 for right click";
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 500;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // Clicker
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(186, 97);
+            this.ControlBox = false;
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(202, 136);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(202, 136);
+            this.Name = "Clicker";
+            this.Text = "Clicker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Clicker/Clicker/Clicker.cs
@@ -0,0 +1,62 @@
+﻿using InputDeviceEventManager;
+using System;
+using System.Windows.Forms;
+
+namespace Clicker
+{
+    public partial class Clicker : Form
+    {
+        private readonly DeviceListener deviceListener = new DeviceListener();
+
+        private bool leftClick = false;
+        private bool rightClick = false;
+
+        public Clicker()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            deviceListener.StartListen();
+            deviceListener.KeyboardKeyDown += DeviceListener_KeyboardKeyDown;
+        }
+
+        private void DeviceListener_KeyboardKeyDown(object sender, InputDeviceEventManager.Keyboard.KeyboardEventArgs keyboardEventArgs)
+        {
+            if(keyboardEventArgs.VirtualKeyCode == InputDeviceEventManager.Keyboard.VirtualKeyCodes.F8)
+            {
+                leftClick = !leftClick;
+            }
+
+            if(keyboardEventArgs.VirtualKeyCode == InputDeviceEventManager.Keyboard.VirtualKeyCodes.F9)
+            {
+                rightClick = !rightClick;
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            deviceListener.StopListen();
+
+            base.OnFormClosing(e);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if(leftClick)
+            {
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftDown);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.LeftUp);
+            }
+
+            if (rightClick)
+            {
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.RightDown);
+                MouseOperations.MouseEvent(MouseOperations.MouseEventFlags.RightUp);
+            }
+        }
+    }
+}
